6cd0f6a442fc52afe3bb5bb0adcdc2af47f70463
Maksim Bulva <mbulva@yandex-team.ru>
2017-04-13 01:44:45 +0300

    Added Players model

--- battlenetclient/models.py
+++ battlenetclient/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Players(models.Model):
+    player_id = models.IntegerField(primary_key=True)
+    display_name = models.CharField(max_length=32, db_index=True)
+    profile_path = models.CharField(max_length=64)
